c2e88d85e2797b90a6b51fce15e1b111ef6380a4
jabin34 <isratjabinesha@gmail.com>
2022-05-06 00:35:27 +0900

    mongo db server added

--- warehouse-server/index.js
+++ warehouse-server/index.js
@@ -2,11 +2,22 @@ const express = require('express');
 const cors = require('cors');
 const app = express(); 
 const port = process.env.PORT || 4000 ;
-//require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
 //middleware 
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5wovs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  console.log("database connected");
+  // perform actions on the collection object
+  client.close();
+});
 
 
 app.get('/',(req,res)=>{
