57b4f3082c6859d57cb52468aa8ff4c3b80206fa
jabin34 <isratjabinesha@gmail.com>
2022-05-05 22:14:00 +0900

    index setup done

---
+++ warehouse-server/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const app = express(); 
+const port = process.env.PORT || 4000 ;
+//require('dotenv').config();
+
+//middleware 
+app.use(cors());
+app.use(express.json());
+
+
+app.get('/',(req,res)=>{
+    res.send("server is running");
+});
+
+//port listening
+
+app.listen(port,()=>{
+    console.log('listening to port',port);
+});
